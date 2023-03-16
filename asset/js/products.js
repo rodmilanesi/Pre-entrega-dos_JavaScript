9a117fd32f19426fb1d491a66c94d4812ee853db
Rod Albarracin <rt.albarracin@mail.com>
2023-03-15 23:23:46 -0300

    Segunda Preentrega Actualización

---
+++ asset/js/products.js
@@ -0,0 +1,44 @@
+const productos = [
+  {
+    id: 1,
+    nombre: "Monitor Ultra WQHD Curve",
+    precioReferencia: 999990,
+    precio: 549990,
+    img: "https://ripleycl.imgix.net/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fim-prod-products-images%2F25474-6060-ls34a650uxlxzs-71193e14-dd8d-494b-87fb-26e4028d1f03.jpg?w=750&h=555&ch=Width&auto=format&cs=strip&bg=FFFFFF&q=60&trimcolor=FFFFFF&trim=color&fit=fillmax&ixlib=js-1.1.0&s=a433c3559f6bc49b6f1fbc82fe7c77d7",
+  },
+  {
+    id: 2,
+    nombre: "Teclado Gamer 40%",
+    precioReferencia: 125990,
+    precio: 99990,
+    img: "https://vgamers.cl/wp-content/uploads/2022/06/2-73.png",
+  },
+  {
+    id: 3,
+    nombre: "Mouse Gamer Turbo Fire",
+    precioReferencia: 112990,
+    precio: 85990,
+    img: "https://casadavinci.cl/5360-large_default/mouse-gamer-usb-7-botones-turbo-fire-3200dpi-reptilex.jpg",
+  },
+  {
+    id: 4,
+    nombre: "Escritorio Elevable E3500",
+    precioReferencia: 549990,
+    precio: 349990,
+    img: "https://m.media-amazon.com/images/I/61PbfVdyWuL._AC_SL1500_.jpg",
+  },
+  {
+    id: 5,
+    nombre: "Luz led rbg gamerzone",
+    precioReferencia: 79990,
+    precio: 34990,
+    img: "https://assets.jumpseller.com/store/govee-cl/themes/413754/options/50599913/2021-10-24T21%3A13%3A21.884Zgovee-tv-gaming.jpg?1635111051",
+  },
+  {
+    id: 6,
+    nombre: "Silla de escritorio",
+    precioReferencia: 119990,
+    precio: 99990,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxYQF2WFRwn6pP5pzwmoF1eeudy8mKjSTojw&usqp=CAU",
+  },
+];
